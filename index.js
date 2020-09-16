04aa41a66896b3b1291b1c45eb5016092c95d1e3
unknown <rahulmaurya032@gmail.com>
2020-09-16 22:03:01 +0530

    some changes

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const togglenav = document.querySelector('.btn-style');
-let data = document.querySelectorAll('.navs-link');
+let data = document.querySelectorAll('.nav-items');
+
 
 togglenav.addEventListener('click' , ()=>{
     document.body.classList.toggle('nav-open')
-    console.log("sidebar oprn")
+    console.log("sidebar oprn");
 })
 
 
-data.forEach(link =>{
-    link.addEventListener('click' ,()=>{
-        document.body.classList.remove('nav-open');
-        console.log("link")
+data.forEach(linkss =>{
+    linkss.addEventListener('click' ,()=>{
+        document.body.classList.remove('nav-open')
     })
 })
